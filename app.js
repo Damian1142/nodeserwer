e20ddfacca26100ff64ae75fb51ce7a11b209a4a
Damian1142 <83139509+Damian1142@users.noreply.github.com>
2022-04-04 23:46:53 +0200

    cm4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express()
 
 app.get('/', function(req,res){
-    res.json('{"status":"200"}')
+    res.json("{\"status\":200}")
 })
 
 app.get('/rcon', function(req,res) {
